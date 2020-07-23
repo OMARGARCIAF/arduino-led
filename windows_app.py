0d43308f357abc59626e703abd56b1c4a3b8d562
JESUS OMAR GARCIA FALCON <54721759+OMARGARCIAF@users.noreply.github.com>
2020-07-23 02:16:45 -0500

    Update windows_app.py

--- windows_app.py
+++ windows_app.py
@@ -1,17 +1,26 @@
 from windows import *
 from arduinoserial import *
+sis="SISTEMA: "
 class MainWindow(QtWidgets.QMainWindow, Ui_PRENDERLED):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)  
-       
         self.boton__activar.clicked.connect(self.on)
         self.boton_desactivar.clicked.connect(self.off)
         self.label_led_on.setHidden(True)
         self.label_2.setHidden(True)
         self.etiqueta.setText("apagado")
+        
         try:
-            self.ubiCOM.addItem(po[:4])
+            if "COM" in po:
+                self.sistemaop.setText(sis+"Windows")
+                self.ubiCOM.addItem(po[:4])
+            elif "/dev" in po:
+                self.sistemaop.setText(sis+"Linux")
+                self.ubiCOM.addItem(po[:12])
+            elif "usbmodem" in po:
+                self.sistemaop.setText(sis+"MAC")
+                self.ubiCOM.addItem(po[:21])
         except:
             self.ubiCOM.addItem("ERROR")
         
@@ -37,7 +46,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_PRENDERLED):
         except:
             self.etiqueta.setText("No se pudo conectar")
             self.label_2.setHidden(False)
-
+    def reset(self):
+        import arduinoserial
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     window = MainWindow()
